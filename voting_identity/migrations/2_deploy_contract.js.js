36cb66ca38b6d6c06a956bf8a013479e283d9b3c
hallelx2 <halleluyaholudele@gmail.com>
2024-02-01 23:50:42 +0100

    made some changes to the views.py in the voting app

--- voting_identity/migrations/2_deploy_contract.js.js
+++ voting_identity/migrations/2_deploy_contract.js.js
@@ -1,7 +1,9 @@
 // migrations/2_deploy_contract.js
 
 const MyContract = artifacts.require("DecentralizedIdentity");
+const VotingContract = artifacts.require("DecentralizedVoting")
 
 module.exports = function(deployer) {
   deployer.deploy(MyContract);
+  deployer.deploy(VotingContract)
 };
